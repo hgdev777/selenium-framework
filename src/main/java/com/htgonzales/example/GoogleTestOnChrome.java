89550d12082eac05e4af8372aeb278636baeb6f5
htgonzales <hernan.gonzales@dominionenterprises.com>
2014-11-14 12:33:27 -0500

    Add test using Chrome example

---
+++ src/main/java/com/htgonzales/example/GoogleTestOnChrome.java
@@ -0,0 +1,42 @@
+package com.htgonzales.example;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import com.htgonzales.util.TestCaseExt;
+
+/**
+ * Test case using chrome driver
+ * 
+ * @author hernan
+ * @version 1.0
+ * 
+ */
+public class GoogleTestOnChrome extends TestCaseExt {
+
+	@BeforeClass
+	public void oneTimeSetUp() throws Exception {
+		Path path = Paths.get(GoogleTestOnChrome.class.getResource(".").toURI());
+		String basePath = path.getParent().getParent().getParent().getParent()
+				.getParent().toString();	
+		System.setProperty("webdriver.chrome.driver", basePath + "/tools/chromedriver");
+		init(new ChromeDriver(), "http://www.google.com");
+	}
+
+	@Test(priority = 1)
+	public void verifyGoogleSearch() throws Exception {
+		
+		add(ID, "hplogo");
+		verify();
+		
+		input(ID, "gbqfq", "selenium");
+		click(ID, "gbqfba");
+		add(LINK_TEXT, "Selenium - Web Browser Automation");
+		add(LINK_TEXT, "Selenium - Wikipedia, the free encyclopedia");
+		verify();
+		
+	}
+
+}
